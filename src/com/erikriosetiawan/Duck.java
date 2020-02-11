da829e96569acd0addec5a65cc47fe751757a2df
erikrios <erikriosetiawan49@gmail.com>
2020-02-11 21:12:13 +0700

    Add some attributes to Duck class

--- src/com/erikriosetiawan/Duck.java
+++ src/com/erikriosetiawan/Duck.java
@@ -2,9 +2,27 @@ package com.erikriosetiawan;
 
 public abstract class Duck {
 
+    // Declare two reference variables for the behavior interface types.
+    // All duck subclasses (in the same package) inherit these.
+    FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
+    public Duck() {
+    }
+
+    public abstract void display();
+
+    // Delegate the behavior class.
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    // Delegate the behavior class.
     public void performQuack() {
         quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
     }
 }
