c5ce88126ada554de09890c0e6a9559fb3ff6b94
erikrios <erikriosetiawan49@gmail.com>
2020-02-11 21:18:57 +0700

    Add FlyWithWings class

---
+++ src/com/erikriosetiawan/FlyWithWings.java
@@ -0,0 +1,10 @@
+package com.erikriosetiawan;
+
+public class FlyWithWings implements FlyBehavior {
+
+    // Flying behavior implementation for ducks that do fly...
+    @Override
+    public void fly() {
+        System.out.println("I'm flying!!");
+    }
+}
