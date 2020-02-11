5de39f2876d1d9573411f3d62c88ff159e269bdd
erikrios <erikriosetiawan49@gmail.com>
2020-02-11 22:08:56 +0700

    Change the test class (MiniDuckSimulator.java), add the ModelDuck, and make the ModelDuck rocket-enabled

--- src/com/erikriosetiawan/MiniDuckSimulator.java
+++ src/com/erikriosetiawan/MiniDuckSimulator.java
@@ -13,5 +13,20 @@ public class MiniDuckSimulator {
         // Then we do the same thing with MallardDuck's
         // inherited performFly() method.
         mallard.performFly();
+
+        Duck model = new ModelDuck();
+
+        // The first call to performFly() delegates to the flyBehavior object
+        // set in the FlyBehavior object set in the ModelDuck's constructor,
+        // which is a FlyNoWay instance.
+        model.performFly();
+
+        // This invokes the model's inherited behavior setter method, and...voila!
+        // The model suddenly has rocket-powered flying capability!
+        model.setFlyBehavior(new FlyRocketPowered());
+
+        // If it worked, the model duck dynamically changed its flying behavior!
+        // You can't do that if the implementation lives inside the duck class.
+        model.performFly();
     }
 }
